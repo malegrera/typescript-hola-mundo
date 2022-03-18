89c00f78f08f4f917f90e08979b3fa034e18ad4f
malegrera <malegrera@uoc.edu>
2022-03-18 22:46:09 +0100

    3 files modified

--- index.ts
+++ index.ts
@@ -3,4 +3,4 @@ import './style.css';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<i class="fa-solid fa-user"></i>`;
+appDiv.innerHTML = `<h1>Hola</h1>`;
