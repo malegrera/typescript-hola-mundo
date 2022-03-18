92bd1f46a0ad957515eb75da277aa6051892e1d4
malegrera <malegrera@uoc.edu>
2022-03-18 23:09:45 +0100

    3 files modified, 1 added

--- index.ts
+++ index.ts
@@ -4,11 +4,16 @@ import "@fortawesome/fontawesome-free/css/all.css";
 
 
 // Write TypeScript code!
-let nombre:string="Pili";
+const usuario:string="palegre";
+const rana:string = `<div class="fa-3x">
+<i class="fa-solid fa-frog fa-bounce" style=" --fa-bounce-start-scale-x: 1; --fa-bounce-start-scale-y: 1; --fa-bounce-jump-scale-x: 1; --fa-bounce-jump-scale-y: 1; --fa-bounce-land-scale-x: 1; --fa-bounce-land-scale-y: 1; " ></i>
+</div>`;
+const appFooter: HTMLElement = document.querySelector('footer');
+appFooter.innerHTML=rana;
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `
-<h1>Hola ${nombre}</h1>
-<i class="fa-solid fa-user"></i>
+<h1><i class="fa-solid fa-user"></i> ${usuario} </h1>
+
 <span style="font-size: 3em; color: Tomato;">
 <i class="fas fa-camera"></i>
 </span>
@@ -19,3 +24,4 @@ appDiv.innerHTML = `
   <i class="fa-solid fa-heart fa-beat" style="--fa-animation-duration: 2s;"></i>
   <i class="fa-solid fa-heart fa-beat" style="--fa-beat-scale: 2.0;"></i>
 </div>`;
+
