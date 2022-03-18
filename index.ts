e4832d0f975b8e15df4c35a817e3454c518e019a
malegrera <malegrera@uoc.edu>
2022-03-18 22:53:38 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -10,4 +10,10 @@ appDiv.innerHTML = `
 <span style="font-size: 3em; color: Tomato;">
 <i class="fas fa-camera"></i>
 </span>
-`;
+<div class="fa-3x">
+  <i class="fa-solid fa-circle-plus fa-beat"></i>
+  <i class="fa-solid fa-heart fa-beat"></i>
+  <i class="fa-solid fa-heart fa-beat" style="--fa-animation-duration: 0.5s;" ></i>
+  <i class="fa-solid fa-heart fa-beat" style="--fa-animation-duration: 2s;"></i>
+  <i class="fa-solid fa-heart fa-beat" style="--fa-beat-scale: 2.0;"></i>
+</div>`;
