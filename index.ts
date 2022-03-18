1548159fb0326fc287307b3e5ca1c9ca19681180
malegrera <malegrera@uoc.edu>
2022-03-18 22:52:50 +0100

    2 files modified

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
 // Import stylesheets
 import './style.css';
+import "@fortawesome/fontawesome-free/css/all.css";
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>Hola</h1>`;
+appDiv.innerHTML = `
+<h1>Hola</h1>
+<i class="fa-solid fa-user"></i>
+<span style="font-size: 3em; color: Tomato;">
+<i class="fas fa-camera"></i>
+</span>
+`;
