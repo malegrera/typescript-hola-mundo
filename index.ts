ff25d74b8ef4237dccdc188066e4e2eaa2b71df1
malegrera <malegrera@uoc.edu>
2022-03-24 03:10:16 +0100

    2 files modified

--- index.ts
+++ index.ts
@@ -8,8 +8,7 @@ const usuario:string="palegre";
 const rana:string = `<div class="fa-3x">
 <i class="fa-solid fa-frog fa-bounce" style=" --fa-bounce-start-scale-x: 1; --fa-bounce-start-scale-y: 1; --fa-bounce-jump-scale-x: 1; --fa-bounce-jump-scale-y: 1; --fa-bounce-land-scale-x: 1; --fa-bounce-land-scale-y: 1; " ></i>
 </div>`;
-const appFooter: HTMLElement = document.querySelector('footer');
-appFooter.innerHTML=rana;
+
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `
 <h1><i class="fa-solid fa-user"></i> ${usuario} </h1>
@@ -23,5 +22,5 @@ appDiv.innerHTML = `
   <i class="fa-solid fa-heart fa-beat" style="--fa-animation-duration: 0.5s;" ></i>
   <i class="fa-solid fa-heart fa-beat" style="--fa-animation-duration: 2s;"></i>
   <i class="fa-solid fa-heart fa-beat" style="--fa-beat-scale: 2.0;"></i>
-</div>`;
+</div><footer>${rana}</footer>`;
 
